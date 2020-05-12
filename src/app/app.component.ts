37ad0d857ced49a408756ec25a4172a454b21f2f
ny.iuriii@gmail.com <ny.iuriii@gmail.com>
2020-05-12 12:57:12 +0200

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = ''
+  date: string = ''
+  amout: number;
+  height: number;
+  miles: number;
+
+  onHeightChange (value:string) {
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange (value:string) {
+    this.miles = parseFloat(value);
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+  onDateChange (value: string) {
+   this.date = value;
+  }
+
+
+  onAmoutChange(value: string) {
+   
+    this.amout = parseFloat(value)
+    
+  }
 }
